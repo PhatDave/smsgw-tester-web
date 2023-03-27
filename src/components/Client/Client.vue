d0e3a8abb14758ad18c6b7202658851ac1fce6eb
574515 <574515c0d1ng@gmail.com>
2023-03-27 12:25:39 +0200

    Made buttons work, added emitters for Jobs, logged status

--- src/components/Client/Client.vue
+++ src/components/Client/Client.vue
@@ -11,6 +11,7 @@ export default {
   },
   beforeMount() {
     this.client.openWebsocket();
+    setInterval(this.checkStatus, 1000);
   },
   methods: {
     deleteClient() {
@@ -20,7 +21,28 @@ export default {
       if (this.client.status === 'NOT CONNECTED') return "bg-danger";
       else if (this.client.status === 'CONNECTED') return "bg-warning";
       else return "bg-success";
-    }
+    },
+    updateSingleJob() {
+      this.client.configDefault();
+
+    },
+    updateMultiJob() {
+      this.client.configureDefaultMany();
+    },
+    connectClient() {
+      this.client.connect();
+    },
+    bindClient() {
+      this.client.bind();
+    },
+    disconnectClient() {
+      this.client.disconnect();
+    },
+    checkStatus() {
+      let currentTimeMillis = Date.now();
+      let currentTime = new Date(currentTimeMillis).toISOString().slice(11, 19);
+      console.log(`${currentTime}: ${this.client.id} is ${this.client.status}`);
+    },
   }
 }
 </script>
@@ -31,13 +53,13 @@ export default {
       <div class="row justify-content-center align-items-center">
         <div class="col-6">{{ client.status }}</div>
         <div class="col-6 text-end">
-          <button v-if="client.status === 'NOT CONNECTED'" class="btn btn-sm btn-warning mx-1">
+          <button v-if="client.status === 'NOT CONNECTED'" class="btn btn-sm btn-warning mx-1" @click="connectClient">
             Connect
           </button>
-          <button v-if="client.status !== 'NOT CONNECTED' && client.status !== 'BOUND'" class="btn btn-sm btn-success mx-1">
+          <button v-if="client.status !== 'NOT CONNECTED' && client.status !== 'BOUND'" class="btn btn-sm btn-success mx-1" @click="bindClient">
             Bind
           </button>
-          <button v-if="client.status === 'CONNECTED' || client.status === 'BOUND'" class="btn btn-sm btn-danger mx-1">
+          <button v-if="client.status === 'CONNECTED' || client.status === 'BOUND'" class="btn btn-sm btn-danger mx-1" @click="disconnectClient">
             Disconnect
           </button>
         </div>
@@ -55,13 +77,13 @@ export default {
           <input type="text" class="form-control" placeholder="Password" :value="client.password" @input="event => client.setPassword(event.target.value)">
         </div>
         <div class="col-2">
-          <button type="button" class="btn btn-sm btn-outline-danger" v-on:dblclick="deleteClient()">Delete</button>
+          <button type="button" class="btn btn-sm btn-outline-danger" @dblclick="deleteClient()">Delete</button>
         </div>
         <!-- TODO: Progress Bar -->
       </div>
       <hr>
       <div class="p-2">
-        <DefaultJob :default-job="client.defaultJob" :default-multi-job="client.defaultMultiJob"/>
+        <DefaultJob :default-job="client.defaultJob" :default-multi-job="client.defaultMultiJob" @singleJob="updateSingleJob" @multiJob="updateMultiJob"/>
       </div>
     </div>
   </div>
