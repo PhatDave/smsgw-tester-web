3c3a04570f7c9721bb3dfe03071631ca6b7c7c86
574515 <574515c0d1ng@gmail.com>
2023-04-06 22:56:16 +0200

    Added dynamic class and margin

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -5,16 +5,30 @@ export default {
 	name: "ActionButton",
 	props: {
 		action: Action
+	},
+	methods: {
+		getClass(actionName) {
+			switch (actionName) {
+				case 'Disconnect':
+					return 'btn-danger'
+				case 'Connect':
+					return 'btn-success'
+				case 'Bind':
+					return 'btn-warning'
+			}
+		}
 	}
 }
 </script>
 
 <template>
-	<button :disabled="!action.active()" @click="action.execute()">
+	<button :class="getClass(action.name)" class="btn btn-sm" :disabled="!action.active()" @click="action.execute()">
 		{{ action.name }}
 	</button>
 </template>
 
 <style scoped>
-
+button {
+	margin: 0 .3rem;
+}
 </style>
