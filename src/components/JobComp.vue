d4e743802ba722395034e4f157f440a96c166fc0
David Majdandžić <david.majdandzic@hotmail.com>
2023-05-05 11:25:47 +0200

    Implement parts computation for long sms (F#58)
    The point is to know how many parts a message has before it's sent

--- src/components/JobComp.vue
+++ src/components/JobComp.vue
@@ -21,6 +21,35 @@ export default {
 			required: true
 		}
 	},
+	data() {
+		return {
+			// This is a dummy object to once be replaced with a real encoding object
+			encoding: {
+				value: "",
+				// 7 for gsm7
+				// 8 for utf8
+				// 16 for utf16 aka ucs2
+				bitsPerChar: 7
+			},
+			totalMaxBits: 1120
+		}
+	},
+	methods: {
+		// TODO: This might be difficult to refactor once support for encodings is added
+		calculatePartsForMessage(): { [key: string]: number } {
+			let maxCharsPerPart = this.totalMaxBits / this.encoding.bitsPerChar;
+
+			return {
+				parts: Math.ceil(this.job.message.length / maxCharsPerPart),
+				chars: this.job.message.length,
+				bits: this.job.message.length * this.encoding.bitsPerChar
+			}
+		},
+		messageTitle(): string {
+			let messageStats = this.calculatePartsForMessage();
+			return `Message (${messageStats.parts} parts, ${messageStats.chars} chars, ${messageStats.bits} bits)`;
+		}
+	}
 }
 </script>
 
@@ -30,7 +59,7 @@ export default {
 		<div class="container">
 			<InputFieldComponent :entity="job" field="source" title="Source"/>
 			<InputFieldComponent :entity="job" field="destination" title="Destination"/>
-			<InputFieldComponent :entity="job" field="message" class="grid-size-2" title="Message"/>
+			<InputFieldComponent :entity="job" field="message" class="grid-size-2" v-bind:title="messageTitle()"/>
 			<InputFieldComponent :entity="job" field="perSecond" v-if="job.isMulti" title="M/S"/>
 			<InputFieldComponent :entity="job" field="count" v-if="job.isMulti" title="Count"/>
 			<ActionButton v-if="!job.isMulti" :action="entity.actions.doSend" class="grid-size-2"/>
