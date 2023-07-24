f69f7b52c8a029944992f70b67eaf668a96b984b
David Majdandžić <dmajdandzic@neos.hr>
2023-07-24 09:25:46 +0200

    Update dependencies

--- electron/electron.js
+++ electron/electron.js
@@ -1,52 +1,49 @@
 // electron/electron.js
 const path = require('path');
 const {
-	app,
-	BrowserWindow
+    app, BrowserWindow
 } = require('electron');
 
 const isDev = process.env.IS_DEV == "true" ? true : false;
 // const isDev = false;
 if (!isDev) {
-	require('./dist/main.js');
+    require('./dist/main.js');
 }
 
 function createWindow() {
-	const mainWindow = new BrowserWindow({
-		                                     width: 1600,
-		                                     height: 950,
-		                                     webPreferences: {
-			                                     webSecurity: false
-		                                     },
-	                                     });
+    const mainWindow = new BrowserWindow({
+        width: 1600, height: 950, webPreferences: {
+            webSecurity: false
+        },
+    });
 
-	mainWindow.loadURL(isDev ? 'http://localhost:5173' : `file://${path.join(__dirname, '../dist/index.html')}`);
-	// Open the DevTools.
-	if (isDev) {
-		mainWindow.webContents.openDevTools();
-	}
+    mainWindow.loadURL(isDev ? 'http://localhost:5173' : `file://${path.join(__dirname, '../dist/index.html')}`);
+    // Open the DevTools.
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 }
 
 app.whenReady().then(() => {
-	createWindow()
-	app.on('activate', function() {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow()
-		}
-	})
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 });
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 process.on('message', data => {
-	if (data === 'graceful-exit') {
-		app.quit()
-	}
+    if (data === 'graceful-exit') {
+        app.quit()
+    }
 });
 process.on('SIGTERM', () => {
-	app.quit()
+    app.quit()
 });
